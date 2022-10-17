1d355b97119aee337be68202ba1e66ac936c32f9
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-10-17 20:50:40 +0200

    Update echo_ser_udp.py

--- labo2.1/resolucion/echo_ser_udp.py
+++ labo2.1/resolucion/echo_ser_udp.py
@@ -4,14 +4,12 @@ import socket
 
 PORT = 50007
 
-"""A COMPLETAR POR EL/LA ESTUDIANTE:
-Crear un socket y asignarle su direccion.
-"""
+s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
+
+s.bind( ('', PORT) )
 
 while True:
-	"""A COMPLETAR POR EL/LA ESTUDIANTE:
-	Recibir un mensaje y responder con el mismo.
-	"""
-"""A COMPLETAR POR EL/LA ESTUDIANTE:
-Cerrar socket.
-"""
+	buf, dir_cli = s.recvfrom( 1024 )
+	s.sendto( buf, dir_cli)
+s.close()
+
