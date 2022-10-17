292b1f292a03499439e7b37eecf4c268fda31a3e
Silvia Arenales <91908502+sarenales@users.noreply.github.com>
2022-10-17 20:49:59 +0200

    Update echo_cli_udp.py

--- labo2.1/resolucion/echo_cli_udp.py
+++ labo2.1/resolucion/echo_cli_udp.py
@@ -4,24 +4,20 @@ import socket, sys
 
 PORT = 50007
 
-# Comprueba que se ha pasado un argumento.
 if len( sys.argv ) != 2:
 	print( "Uso: {} <servidor>".format( sys.argv[0] ) )
 	exit( 1 )
 
-"""A COMPLETAR POR EL/LA ESTUDIANTE:
-Crear el socket.
-"""
+dir_serv = (sys.argv[1], PORT)
+
+s = socket.socket( socket.AF_INET, socket.SOCK_DGRAM )
 
 print( "Introduce el mensaje que quieres enviar (mensaje vacío para terminar):" )
 while True:
 	mensaje = input()
 	if not mensaje:
 		break
-	"""A COMPLETAR POR EL/LA ESTUDIANTE:
-	Enviar mensaje y recibir 'eco'.
-	Mostrar en pantalla lo recibido.
-	"""
-"""A COMPLETAR POR EL/LA ESTUDIANTE:
-Cerrar socket.
-"""
+	s.sendto( mensaje.encode(), dir_serv )
+	buf = s.recv( len(mensaje.encode()) )
+	print( buf.decode() )
+s.close()
